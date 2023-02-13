6c55f82e4b615e153cfcd2558ae7e1bd141f3430
jlvargasme <jlvm.ind@hotmail.com>
2023-02-13 11:47:08 -0400

    Updating github actions

--- github-actions-push.sh
+++ github-actions-push.sh
@@ -1,7 +1,8 @@
 echo Running MARVeLus CI/CD
+eval $(opam env)
 ./configure
-opam exec -- dune build --root .
-opam exec -- dune install --root .
+opam exec -- dune build @install --root .
+# opam exec -- dune install --root .
 cd ./test/marvelus
 STR=$(make 2>&1)
 SUB='Error'
