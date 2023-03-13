dc208f9c0571f36db67e5037562c9e184dcc5434
zyicheng <zyicheng@umich.edu>
2023-03-13 02:41:11 -0400

    Testing basic script use

--- github-actions-push.sh
+++ github-actions-push.sh
@@ -1,13 +1,13 @@
 echo Running MARVeLus CI/CD
-eval $(opam env)
-./configure
-opam exec -- dune build @install --root .
-# opam exec -- dune install --root .
-cd ./test/marvelus
-STR=$(make 2>&1)
-SUB='Error'
+# eval $(opam env)
+# ./configure
+# opam exec -- dune build @install --root .
+# # opam exec -- dune install --root .
+# cd ./test/marvelus
+# STR=$(make 2>&1)
+# SUB='Error'
 
-if [[ ${STR} ]];  then
-  echo Error;
-  exit 1
-fi
+# if [[ ${STR} ]];  then
+#   echo Error;
+#   exit 1
+# fi
